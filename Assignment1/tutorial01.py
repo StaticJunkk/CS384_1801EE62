1b014ac0f57d249fc4e9112baffd6a5fbe3a91d9
Yuvi Dhelawat <yuvidhelawat@gmail.com>
2020-09-16 22:19:54 +0530

    Added the four new functions from skeleton

--- Assignment1/tutorial01.py
+++ Assignment1/tutorial01.py
@@ -3,7 +3,7 @@ def add(num1, num2):
     try:
         num1 = int(num1)
         num2 = int(num2)
-        addition = int(num1 + num2)
+        addition = num1 + num2
         return addition
     except ValueError:
         raise ValueError('Works only with numbers')
@@ -16,7 +16,7 @@ def subtract(num1, num2):
     try:
         num1 = int(num1)
         num2 = int(num2)
-        subtraction = int(num1 - num2)
+        subtraction = num1 - num2
         return subtraction
     except ValueError:
         raise ValueError('Works only with numbers')
@@ -27,7 +27,7 @@ def multiply(num1, num2):
     try:
         num1 = int(num1)
         num2 = int(num2)
-        multiplication = int(num1*num2)
+        multiplication = num1*num2
         return multiplication
     except ValueError:
         raise ValueError('Works only with numbers')
@@ -44,3 +44,34 @@ def divide(num1, num2):
         return division
     except ValueError:
         raise ValueError('Works only with numbers')
+
+
+# Function to add power function
+# You cant use the inbuilt python function x ** y . Write your own function
+def power(num1, num2):  # num1 ^ num2
+    # DivisionLogic
+    return power
+
+# Python 3 program to print GP.  geometric Progression
+# You cant use the inbuilt python function. Write your own function
+
+
+def printGP(a, r, n):
+    gp = []
+    return gp
+
+# Python 3 program to print AP.  arithmetic Progression
+# You cant use the inbuilt python function. Write your own function
+
+
+def printAP(a, d, n):
+    ap = []
+    return ap
+
+# Python 3 program to print HP.   Harmonic Progression
+# You cant use the inbuilt python function. Write your own function
+
+
+def printHP(a, d, n):
+    hp = []
+    return hp
