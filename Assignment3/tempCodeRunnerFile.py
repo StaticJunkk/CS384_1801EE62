f12ce9403182debd95e1f4da07f2fc57090c9c48
Yuvi Dhelawat <yuvidhelawat@gmail.com>
2020-10-14 16:03:52 +0530

    Created folders for all the branches, ignoring cs folder as it already existed

---
+++ Assignment3/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+for rows in reader:
---
+++ Assignment3/tutorial03.py
@@ -0,0 +1,61 @@
+import csv
+import os
+import re
+os.system('cls')
+path = os.getcwd()
+
+
+def course():
+    dir_path = os.path.join(path, r'analytics\course')
+    courses = ('cs', 'ee', 'me', 'cb', 'ce', 'ms', 'ph',
+               'ma', 'hs', 'ch', 'nt', 'mt', 'ms', 'mc')
+    for course_name in courses:
+        new_path = os.path.join(dir_path, course_name)
+        if os.path.exists(new_path):
+            pass
+        else:
+            os.mkdir(new_path)
+    try:
+        with open('studentinfo_cs384.csv', 'r') as info_file:
+            reader = csv.DictReader(info_file)
+            # for rows in reader:
+    except:
+        print('Error while reading CSV file')
+
+
+course()
+
+# def country():
+#     # Read csv and process
+#     pass
+
+
+# def email_domain_extract():
+#     # Read csv and process
+#     pass
+
+
+# def gender():
+#     # Read csv and process
+#     pass
+
+
+# def dob():
+#     # Read csv and process
+#     pass
+
+
+# def state():
+#     # Read csv and process
+#     pass
+
+
+# def blood_group():
+#     # Read csv and process
+#     pass
+
+
+# # Create the new file here and also sort it in this function only.
+# def new_file_sort():
+#     # Read csv and process
+#     pass
