1f7ca5a9303a21b81aba02bd0da26a42f29a8e90
Yuvi Dhelawat <yuvidhelawat@gmail.com>
2020-10-14 19:48:14 +0530

    Completed DOB task, sorted data for various birth years, added condition for missing dob field rows to be saved in misc.csv

--- Assignment3/tutorial03.py
+++ Assignment3/tutorial03.py
@@ -229,9 +229,123 @@ def gender():
         print("Error in reading CSV file")
 
 
-# def dob():
-#     # Read csv and process
-#     pass
+def dob():
+    try:
+        dir_path = os.path.join(direct_path, r'dob')
+        if os.path.isdir(dir_path):
+            pass
+        else:
+            os.mkdir(dir_path)
+        with open('studentinfo_cs384.csv', 'r') as info_file:
+            reader = csv.DictReader(info_file)
+            for row in reader:
+                birth_date = row['dob']
+                date, month, year = re.split(r'-', birth_date)
+
+                if int(year) <= 1999 and int(year) >= 1995:
+
+                    file_name = 'bday_1995_1999.csv'
+                    file_path = os.path.join(dir_path, file_name)
+                    if os.path.isfile(file_path):
+                        with open(file_path, 'a+', newline='') as file:
+                            writer = csv.DictWriter(file, fieldnames=[
+                                'id', 'full_name', 'country', 'email', 'gender', 'dob', 'blood_group', 'state'])
+                            writer.writerow(row)
+                            file.close()
+                    else:
+                        with open(file_path, 'a+', newline='') as file:
+                            writer = csv.DictWriter(file, fieldnames=[
+                                                    'id', 'full_name', 'country', 'email', 'gender', 'dob', 'blood_group', 'state'])
+                            writer.writeheader()
+                            writer.writerow(row)
+                            file.close()
+                elif int(year) <= 2004 and int(year) >= 2000:
+
+                    file_name = 'bday_2000_2004.csv'
+                    file_path = os.path.join(dir_path, file_name)
+                    if os.path.isfile(file_path):
+                        with open(file_path, 'a+', newline='') as file:
+                            writer = csv.DictWriter(file, fieldnames=[
+                                'id', 'full_name', 'country', 'email', 'gender', 'dob', 'blood_group', 'state'])
+                            writer.writerow(row)
+                            file.close()
+                    else:
+                        with open(file_path, 'a+', newline='') as file:
+                            writer = csv.DictWriter(file, fieldnames=[
+                                                    'id', 'full_name', 'country', 'email', 'gender', 'dob', 'blood_group', 'state'])
+                            writer.writeheader()
+                            writer.writerow(row)
+                            file.close()
+                elif int(year) <= 2009 and int(year) >= 2005:
+
+                    file_name = 'bday_2005_2009.csv'
+                    file_path = os.path.join(dir_path, file_name)
+                    if os.path.isfile(file_path):
+                        with open(file_path, 'a+', newline='') as file:
+                            writer = csv.DictWriter(file, fieldnames=[
+                                'id', 'full_name', 'country', 'email', 'gender', 'dob', 'blood_group', 'state'])
+                            writer.writerow(row)
+                            file.close()
+                    else:
+                        with open(file_path, 'a+', newline='') as file:
+                            writer = csv.DictWriter(file, fieldnames=[
+                                                    'id', 'full_name', 'country', 'email', 'gender', 'dob', 'blood_group', 'state'])
+                            writer.writeheader()
+                            writer.writerow(row)
+                            file.close()
+                elif int(year) <= 2014 and int(year) >= 2010:
+
+                    file_name = 'bday_2010_2014.csv'
+                    file_path = os.path.join(dir_path, file_name)
+                    if os.path.isfile(file_path):
+                        with open(file_path, 'a+', newline='') as file:
+                            writer = csv.DictWriter(file, fieldnames=[
+                                'id', 'full_name', 'country', 'email', 'gender', 'dob', 'blood_group', 'state'])
+                            writer.writerow(row)
+                            file.close()
+                    else:
+                        with open(file_path, 'a+', newline='') as file:
+                            writer = csv.DictWriter(file, fieldnames=[
+                                                    'id', 'full_name', 'country', 'email', 'gender', 'dob', 'blood_group', 'state'])
+                            writer.writeheader()
+                            writer.writerow(row)
+                            file.close()
+                elif int(year) <= 2020 and int(year) >= 2015:
+
+                    file_name = 'bday_2015_2020.csv'
+                    file_path = os.path.join(dir_path, file_name)
+                    if os.path.isfile(file_path):
+                        with open(file_path, 'a+', newline='') as file:
+                            writer = csv.DictWriter(file, fieldnames=[
+                                'id', 'full_name', 'country', 'email', 'gender', 'dob', 'blood_group', 'state'])
+                            writer.writerow(row)
+                            file.close()
+                    else:
+                        with open(file_path, 'a+', newline='') as file:
+                            writer = csv.DictWriter(file, fieldnames=[
+                                                    'id', 'full_name', 'country', 'email', 'gender', 'dob', 'blood_group', 'state'])
+                            writer.writeheader()
+                            writer.writerow(row)
+                            file.close()
+                else:
+                    file_name = 'misc.csv'
+                    file_path = os.path.join(dir_path, file_name)
+                    if os.path.isfile(file_path):
+                        with open(file_path, 'a+', newline='') as file:
+                            writer = csv.DictWriter(file, fieldnames=[
+                                'id', 'full_name', 'country', 'email', 'gender', 'dob', 'blood_group', 'state'])
+                            writer.writerow(row)
+                            file.close()
+                    else:
+                        with open(file_path, 'a+', newline='') as file:
+                            writer = csv.DictWriter(file, fieldnames=[
+                                                    'id', 'full_name', 'country', 'email', 'gender', 'dob', 'blood_group', 'state'])
+                            writer.writeheader()
+                            writer.writerow(row)
+                            file.close()
+
+    except:
+        print("Error in reading CSV file")
 
 
 # def state():
@@ -248,8 +362,8 @@ def gender():
 # def new_file_sort():
 #     # Read csv and process
 #     pass
-
 # course()
 # country()
 # gender()
-email_domain_extract()
+# email_domain_extract()
+dob()
