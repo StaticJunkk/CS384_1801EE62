a9a3aaa2021515f404e36f651269bbfd89518cf7
Yuvi Dhelawat <yuvidhelawat@gmail.com>
2020-11-10 23:37:05 +0530

    Changes made in overall function to accomodate missing semesters

--- Assignment/Assignment 5 Web Series Renamer/tutorial05.py
+++ Assignment/Assignment 5 Web Series Renamer/tutorial05.py
@@ -142,72 +142,72 @@ def rename_How_I_Met_Your_Mother(folder_name):
 
 subtitle_path = os.path.join(os.getcwd(), 'Subtitles')
 
-# ch = 0
-# while ch == 0:
-#     x = input('''Select the series name:
-#     1. FIR
-#     2. Game of Thrones
-#     3. Sherlock
-#     4. Suits
-#     5. How I Met Your Mother
-#     ''')
-#     print(type(x))
-#     valid_entries = ['1', '2', '3', '4', '5']
-#     if x not in valid_entries:
-#         print(
-#             f"\nError: You entered -> {x}\n{x} is an Invalid Entry\nPlease enter a valid entry\n")
-#     else:
-#         print('You have entered a valid input\n----------------------------------------\n')
-#         check_season_padding = 0
-#         while check_season_padding == 0:
-#             global season_padding
-#             season_padding = input(
-#                 "Please select padding for season number -> ")
-#             try:
-#                 season_padding = float(season_padding)
-#                 check_season_padding = 1
-#             except:
-#                 print(
-#                     'Invalid Season Padding\nPlease enter again\n----------------------------------------\n')
-#         check_episode_padding = 0
-#         while check_episode_padding == 0:
-#             global episode_padding
-#             episode_padding = input(
-#                 "Please select padding for episode number -> ")
-#             try:
-#                 episode_padding = float(episode_padding)
-#                 check_episode_padding = 1
-#             except:
-#                 print(
-#                     'Invalid episode Padding\nPlease enter again\n----------------------------------------\n')
-#         ch = 1
-#         if x == '1':
-#             folder_path = os.path.join(subtitle_path, 'FIR')
-#             rename_FIR(folder_path)
-#         elif x == '2':
-#             folder_path = os.path.join(subtitle_path, 'Game of Thrones')
-#             rename_Game_of_Thrones(folder_path)
-#         elif x == '3':
-#             folder_path = os.path.join(subtitle_path, 'Sherlock')
-#             rename_Sherlock(folder_path)
-#         elif x == '4':
-#             folder_path = os.path.join(subtitle_path, 'Suits')
-#             deleted_file = rename_Suits(folder_path)
-#             print(f"Total Duplicate files removed -> {deleted_file}")
-#         else:
-#             folder_path = os.path.join(subtitle_path, 'How I Met Your Mother')
-#             deleted_file = rename_How_I_Met_Your_Mother(folder_path)
-#             print(f"Total Duplicate files removed -> {deleted_file}")
+ch = 0
+while ch == 0:
+    x = input('''Select the series name:
+    1. FIR
+    2. Game of Thrones
+    3. Sherlock
+    4. Suits
+    5. How I Met Your Mother
+    ''')
+    print(type(x))
+    valid_entries = ['1', '2', '3', '4', '5']
+    if x not in valid_entries:
+        print(
+            f"\nError: You entered -> {x}\n{x} is an Invalid Entry\nPlease enter a valid entry\n")
+    else:
+        print('You have entered a valid input\n----------------------------------------\n')
+        check_season_padding = 0
+        while check_season_padding == 0:
+            global season_padding
+            season_padding = input(
+                "Please select padding for season number -> ")
+            try:
+                season_padding = float(season_padding)
+                check_season_padding = 1
+            except:
+                print(
+                    'Invalid Season Padding\nPlease enter again\n----------------------------------------\n')
+        check_episode_padding = 0
+        while check_episode_padding == 0:
+            global episode_padding
+            episode_padding = input(
+                "Please select padding for episode number -> ")
+            try:
+                episode_padding = float(episode_padding)
+                check_episode_padding = 1
+            except:
+                print(
+                    'Invalid episode Padding\nPlease enter again\n----------------------------------------\n')
+        ch = 1
+        if x == '1':
+            folder_path = os.path.join(subtitle_path, 'FIR')
+            rename_FIR(folder_path)
+        elif x == '2':
+            folder_path = os.path.join(subtitle_path, 'Game of Thrones')
+            rename_Game_of_Thrones(folder_path)
+        elif x == '3':
+            folder_path = os.path.join(subtitle_path, 'Sherlock')
+            rename_Sherlock(folder_path)
+        elif x == '4':
+            folder_path = os.path.join(subtitle_path, 'Suits')
+            deleted_file = rename_Suits(folder_path)
+            print(f"Total Duplicate files removed -> {deleted_file}")
+        else:
+            folder_path = os.path.join(subtitle_path, 'How I Met Your Mother')
+            deleted_file = rename_How_I_Met_Your_Mother(folder_path)
+            print(f"Total Duplicate files removed -> {deleted_file}")
 
-global episode_padding
-episode_padding = 2
-global season_padding
-season_padding = 2
-folder_path = os.path.join(subtitle_path, 'How I Met Your Mother')
-deleted_file = rename_How_I_Met_Your_Mother(folder_path)
-folder_path = os.path.join(subtitle_path, 'Suits')
-deleted_file = rename_Suits(folder_path)
-folder_path = os.path.join(subtitle_path, 'Game of Thrones')
-rename_Game_of_Thrones(folder_path)
-folder_path = os.path.join(subtitle_path, 'Sherlock')
-rename_Sherlock(folder_path)
+# global episode_padding
+# episode_padding = 2
+# global season_padding
+# season_padding = 2
+# folder_path = os.path.join(subtitle_path, 'How I Met Your Mother')
+# deleted_file = rename_How_I_Met_Your_Mother(folder_path)
+# folder_path = os.path.join(subtitle_path, 'Suits')
+# deleted_file = rename_Suits(folder_path)
+# folder_path = os.path.join(subtitle_path, 'Game of Thrones')
+# rename_Game_of_Thrones(folder_path)
+# folder_path = os.path.join(subtitle_path, 'Sherlock')
+# rename_Sherlock(folder_path)
